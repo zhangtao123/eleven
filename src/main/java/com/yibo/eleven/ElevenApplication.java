c00480ef6023543d70163387ff4b7d24852a1a79
zhangtao <zhangtao@anji-plus.com>
2018-10-10 16:06:52 +0800

    初始化

---
+++ src/main/java/com/yibo/eleven/ElevenApplication.java
@@ -0,0 +1,12 @@
+package com.yibo.eleven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevenApplication.class, args);
+	}
+}
---
+++ src/main/java/com/yibo/eleven/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.yibo.eleven.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping("/test/{name}")
+    public Map<String, String> test(@PathVariable("name") String name) {
+        return Map.of("hello   ", name);
+    }
+}
